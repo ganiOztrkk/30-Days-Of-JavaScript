b5b1311af0e4c4086362251a5f80028a6ec251fd
Gani Ozturk <127308866+ganiOztrkk@users.noreply.github.com>
2023-05-18 00:52:21 +0300

    1st Day Finished
    17.05.2023 first day finished
    
    1/30

---
+++ Day1/exercise.js
@@ -0,0 +1,14 @@
+//comment make code readable
+// welcome to 30 days of js challange
+
+/*
+comments can make code readable, easy to reuse and informative
+ */
+
+let name = 'gani'
+let age = 26
+let isMarried = false
+let job
+let salary = null
+
+console.log(typeof age)
---
+++ Day1/variable.js
@@ -0,0 +1,16 @@
+// Declaring different variables of different data types
+let firstName = 'Asabeneh' // first name of a person
+let lastName = 'Yetayeh' // last name of a person
+let country = 'Finland' // country
+let city = 'Helsinki' // capital city
+let age1 = 100 // age in years
+let isMarried = true
+
+console.log(firstName, lastName, country, city, age1, isMarried)
+
+// Declaring variables with number values
+let age = 100 // age in years
+const gravity = 9.81 // earth gravity  in m/s2
+const boilingPoint = 100 // water boiling point, temperature in °C
+const PI = 3.14 // geometrical constant
+console.log(gravity, boilingPoint, PI)
